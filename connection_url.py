0ef66b1ff8fc26800e41fd7cf51c1756bbb3117a
Vimal Aravindashan <vimal.aravindashan@gmail.com>
2016-06-15 20:56:48 +0000

    Update references to dj_database_url to connection_url Rename and update unittests

--- connection_url.py
+++ connection_url.py
@@ -1,30 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import os
+from uuid import uuid4
 
 try:
     import urlparse
 except ImportError:
     import urllib.parse as urlparse
 
+try:
+    from django.core.exceptions import ImproperlyConfigured
+except ImportError:
+    class ImproperlyConfigured(Exception):
+        pass
 
-# Register database schemes in URLs.
-urlparse.uses_netloc.append('postgres')
-urlparse.uses_netloc.append('postgresql')
-urlparse.uses_netloc.append('pgsql')
-urlparse.uses_netloc.append('postgis')
-urlparse.uses_netloc.append('mysql')
-urlparse.uses_netloc.append('mysql2')
-urlparse.uses_netloc.append('mysqlgis')
-urlparse.uses_netloc.append('mysql-connector')
-urlparse.uses_netloc.append('spatialite')
-urlparse.uses_netloc.append('sqlite')
-urlparse.uses_netloc.append('oracle')
-urlparse.uses_netloc.append('oraclegis')
 
-DEFAULT_ENV = 'DATABASE_URL'
-
-SCHEMES = {
+DB_SCHEMES = {
     'postgres': 'django.db.backends.postgresql_psycopg2',
     'postgresql': 'django.db.backends.postgresql_psycopg2',
     'pgsql': 'django.db.backends.postgresql_psycopg2',
@@ -39,82 +30,137 @@ SCHEMES = {
     'oraclegis': 'django.contrib.gis.db.backends.oracle',
 }
 
+CACHE_SCHEMES = {
+    'locmem': 'django.core.cache.backends.locmem.LocMemCache',
+    'memcached': 'django.core.cache.backends.memcached.MemcachedCache',
+    'redis': 'redis_cache.RedisCache',
+}
 
-def config(env=DEFAULT_ENV, default=None, engine=None, conn_max_age=0):
-    """Returns configured DATABASE dictionary from DATABASE_URL."""
+def config(env, *args, **kwargs):
+    """Returns configured dictionary object from env var."""
 
     config = {}
 
-    s = os.environ.get(env, default)
+    cnxn_url = os.environ.get(env, env)
 
-    if s:
-        config = parse(s, engine, conn_max_age)
+    if isinstance(cnxn_url, str):
+        if cnxn_url == 'sqlite://:memory:':
+            # this is a special case, because if we pass this URL into
+            # urlparse, urlparse will choke trying to interpret "memory"
+            # as a port number
+            return {
+                'ENGINE': DB_SCHEMES['sqlite'],
+                'NAME': ':memory:'
+            }
+            # note: no other settings are required for sqlite
+        # otherwise parse the url as normal
+
+        # Register database/cache schemes in URLs.
+        uses_netloc = urlparse.uses_netloc[:]
+        urlparse.uses_netloc.extend(DB_SCHEMES.keys())
+        urlparse.uses_netloc.extend(CACHE_SCHEMES.keys())
+
+        # Parse connection URL.
+        url = urlparse.urlparse(cnxn_url)
+        # Split query strings from path.
+        path = url.path[1:]
+        if '?' in path and not url.query:
+            path, query = path.split('?', 2)
+        else:
+            path, query = path, url.query
+        query = urlparse.parse_qs(query)
+
+        # Unregister database/cache schemes in URLs.
+        urlparse.uses_netloc = uses_netloc
+
+        # Update with environment configuration.
+        if url.scheme in DB_SCHEMES:
+            config = config_db(url, path)
+        elif url.scheme in CACHE_SCHEMES:
+            config = config_cache(url, path)
+        else:
+            raise ImproperlyConfigured(
+                    "Cannot resolve connection URL scheme '%s' into engine. Choices are: %s" %
+                    (url.scheme, ', '.join(sorted(DB_SCHEMES.keys()) + sorted(CACHE_SCHEMES.keys()))))
+
+        # Pass the query string into OPTIONS.
+        options = {}
+        for key, values in query.iteritems():
+            options[key] = values[-1]
+
+        if options:
+            # Support for Postgres Schema URLs
+            if 'currentSchema' in options and config['ENGINE'] == 'django.db.backends.postgresql_psycopg2':
+                options['options'] = '-c search_path={0}'.format(options['currentSchema'])
+            config['OPTIONS'] = options
+
+    # Use dict args to set default values if not already set
+    for arg in args:
+        if isinstance(arg, dict):
+            default = arg.copy()
+            default_options = arg.pop('OPTIONS', {}).copy()
+            default_options.update(config.pop('OPTIONS', {}))
+            default.update(config)
+            if default_options:
+                default['OPTIONS'] = default_options
+            config = default
+
+    # Use kwargs to override any parsed values
+    for cfg,val in kwargs.iteritems():
+        config[cfg] = val
 
     return config
 
 
-def parse(url, engine=None, conn_max_age=0):
+def config_db(url, path):
     """Parses a database URL."""
 
-    if url == 'sqlite://:memory:':
-        # this is a special case, because if we pass this URL into
-        # urlparse, urlparse will choke trying to interpret "memory"
-        # as a port number
-        return {
-            'ENGINE': SCHEMES['sqlite'],
-            'NAME': ':memory:'
-        }
-        # note: no other settings are required for sqlite
-
-    # otherwise parse the url as normal
     config = {}
-
-    url = urlparse.urlparse(url)
-
-    # Split query strings from path.
-    path = url.path[1:]
-    if '?' in path and not url.query:
-        path, query = path.split('?', 2)
-    else:
-        path, query = path, url.query
-    query = urlparse.parse_qs(query)
 
     # If we are using sqlite and we have no path, then assume we
     # want an in-memory database (this is the behaviour of sqlalchemy)
     if url.scheme == 'sqlite' and path == '':
         path = ':memory:'
 
-    # Handle postgres percent-encoded paths.
-    hostname = url.hostname or ''
-    if '%2f' in hostname.lower():
-        hostname = hostname.replace('%2f', '/').replace('%2F', '/')
-
     # Update with environment configuration.
     config.update({
+        'ENGINE': DB_SCHEMES.get(url.scheme, ''),
         'NAME': urlparse.unquote(path or ''),
         'USER': urlparse.unquote(url.username or ''),
         'PASSWORD': urlparse.unquote(url.password or ''),
-        'HOST': hostname,
+        'HOST': (url.hostname or '').replace('%2f', '/').replace('%2F', '/'),
         'PORT': url.port or '',
-        'CONN_MAX_AGE': conn_max_age,
     })
 
-    # Lookup specified engine.
-    engine = SCHEMES[url.scheme] if engine is None else engine
+    return config
 
-    # Pass the query string into OPTIONS.
-    options = {}
-    for key, values in query.items():
-        options[key] = values[-1]
 
-    # Support for Postgres Schema URLs
-    if 'currentSchema' in options and engine == 'django.db.backends.postgresql_psycopg2':
-        options['options'] = '-c search_path={0}'.format(options['currentSchema'])
+def config_cache(url, path):
+    """Parses a cache URL."""
 
-    if options:
-        config['OPTIONS'] = options
+    config = {}
 
-    if engine:
-        config['ENGINE'] = engine
+    # if we are using locmem and we have no hostname,
+    # then assume we want a random unique id
+    if url.scheme == 'locmem' and not path:
+        path = uuid4().hex
+    else:
+        path = '%(host)s%(port)s%(path)s' % {
+            'host': url.hostname,
+            'port': (':%s' % url.port) if url.port else '',
+            'path': (':%s' % url.path) if url.path else '',
+        }
+
+    # Update with environment configuration.
+    config.update({
+        'BACKEND': CACHE_SCHEMES.get(url.scheme, ''),
+        'LOCATION': path
+    })
+    if url.password:
+        config.update({
+            'OPTIONS': {
+                'PASSWORD': url.password,
+            }
+        })
 
     return config
--- test_connection_url.py
+++ test_connection_url.py
@@ -4,7 +4,7 @@
 import os
 import unittest
 
-import dj_database_url
+import connection_url
 
 
 POSTGIS_URL = 'postgis://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn'
@@ -12,9 +12,9 @@ POSTGIS_URL = 'postgis://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compu
 
 class DatabaseTestSuite(unittest.TestCase):
 
-    def test_postgres_parsing(self):
+    def test_postgres(self):
         url = 'postgres://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn'
-        url = dj_database_url.parse(url)
+        url = connection_url.config(url)
 
         assert url['ENGINE'] == 'django.db.backends.postgresql_psycopg2'
         assert url['NAME'] == 'd8r82722r2kuvn'
@@ -23,9 +23,9 @@ class DatabaseTestSuite(unittest.TestCase):
         assert url['PASSWORD'] == 'wegauwhgeuioweg'
         assert url['PORT'] == 5431
 
-    def test_postgres_unix_socket_parsing(self):
+    def test_postgres_unix_socket(self):
         url = 'postgres://%2Fvar%2Frun%2Fpostgresql/d8r82722r2kuvn'
-        url = dj_database_url.parse(url)
+        url = connection_url.config(url)
 
         assert url['ENGINE'] == 'django.db.backends.postgresql_psycopg2'
         assert url['NAME'] == 'd8r82722r2kuvn'
@@ -34,9 +34,9 @@ class DatabaseTestSuite(unittest.TestCase):
         assert url['PASSWORD'] == ''
         assert url['PORT'] == ''
 
-    def test_postgres_search_path_parsing(self):
+    def test_postgres_search_path(self):
         url = 'postgres://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn?currentSchema=otherschema'
-        url = dj_database_url.parse(url)
+        url = connection_url.config(url)
         assert url['ENGINE'] == 'django.db.backends.postgresql_psycopg2'
         assert url['NAME'] == 'd8r82722r2kuvn'
         assert url['HOST'] == 'ec2-107-21-253-135.compute-1.amazonaws.com'
@@ -46,9 +46,9 @@ class DatabaseTestSuite(unittest.TestCase):
         assert url['OPTIONS']['options'] == '-c search_path=otherschema'
 
 
-    def test_postgres_parsing_with_special_characters(self):
+    def test_postgres_with_special_characters(self):
         url = 'postgres://%23user:%23password@ec2-107-21-253-135.compute-1.amazonaws.com:5431/%23database'
-        url = dj_database_url.parse(url)
+        url = connection_url.config(url)
 
         assert url['ENGINE'] == 'django.db.backends.postgresql_psycopg2'
         assert url['NAME'] == '#database'
@@ -57,9 +57,9 @@ class DatabaseTestSuite(unittest.TestCase):
         assert url['PASSWORD'] == '#password'
         assert url['PORT'] == 5431
 
-    def test_postgis_parsing(self):
+    def test_postgis(self):
         url = 'postgis://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn'
-        url = dj_database_url.parse(url)
+        url = connection_url.config(url)
 
         assert url['ENGINE'] == 'django.contrib.gis.db.backends.postgis'
         assert url['NAME'] == 'd8r82722r2kuvn'
@@ -68,9 +68,9 @@ class DatabaseTestSuite(unittest.TestCase):
         assert url['PASSWORD'] == 'wegauwhgeuioweg'
         assert url['PORT'] == 5431
 
-    def test_mysql_gis_parsing(self):
+    def test_mysql_gis(self):
         url = 'mysqlgis://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn'
-        url = dj_database_url.parse(url)
+        url = connection_url.config(url)
 
         assert url['ENGINE'] == 'django.contrib.gis.db.backends.mysql'
         assert url['NAME'] == 'd8r82722r2kuvn'
@@ -79,9 +79,9 @@ class DatabaseTestSuite(unittest.TestCase):
         assert url['PASSWORD'] == 'wegauwhgeuioweg'
         assert url['PORT'] == 5431
 
-    def test_mysql_connector_parsing(self):
+    def test_mysql_connector(self):
         url = 'mysql-connector://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn'
-        url = dj_database_url.parse(url)
+        url = connection_url.config(url)
 
         assert url['ENGINE'] == 'mysql.connector.django'
         assert url['NAME'] == 'd8r82722r2kuvn'
@@ -90,100 +90,23 @@ class DatabaseTestSuite(unittest.TestCase):
         assert url['PASSWORD'] == 'wegauwhgeuioweg'
         assert url['PORT'] == 5431
 
-    def test_cleardb_parsing(self):
-        url = 'mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true'
-        url = dj_database_url.parse(url)
-
-        assert url['ENGINE'] == 'django.db.backends.mysql'
-        assert url['NAME'] == 'heroku_97681db3eff7580'
-        assert url['HOST'] == 'us-cdbr-east.cleardb.com'
-        assert url['USER'] == 'bea6eb025ca0d8'
-        assert url['PASSWORD'] == '69772142'
-        assert url['PORT'] == ''
-
-    def test_database_url(self):
-        del os.environ['DATABASE_URL']
-        a = dj_database_url.config()
-        assert not a
-
-        os.environ['DATABASE_URL'] = 'postgres://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn'
-
-        url = dj_database_url.config()
-
-        assert url['ENGINE'] == 'django.db.backends.postgresql_psycopg2'
-        assert url['NAME'] == 'd8r82722r2kuvn'
-        assert url['HOST'] == 'ec2-107-21-253-135.compute-1.amazonaws.com'
-        assert url['USER'] == 'uf07k1i6d8ia0v'
-        assert url['PASSWORD'] == 'wegauwhgeuioweg'
-        assert url['PORT'] == 5431
-
     def test_empty_sqlite_url(self):
         url = 'sqlite://'
-        url = dj_database_url.parse(url)
+        url = connection_url.config(url)
 
         assert url['ENGINE'] == 'django.db.backends.sqlite3'
         assert url['NAME'] == ':memory:'
 
     def test_memory_sqlite_url(self):
         url = 'sqlite://:memory:'
-        url = dj_database_url.parse(url)
+        url = connection_url.config(url)
 
         assert url['ENGINE'] == 'django.db.backends.sqlite3'
         assert url['NAME'] == ':memory:'
 
-    def test_parse_engine_setting(self):
-        engine = 'django_mysqlpool.backends.mysqlpool'
-        url = 'mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true'
-        url = dj_database_url.parse(url, engine)
-
-        assert url['ENGINE'] == engine
-
-    def test_config_engine_setting(self):
-        engine = 'django_mysqlpool.backends.mysqlpool'
-        os.environ['DATABASE_URL'] = 'mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true'
-        url = dj_database_url.config(engine=engine)
-
-        assert url['ENGINE'] == engine
-
-    def test_parse_conn_max_age_setting(self):
-        conn_max_age = 600
-        url = 'mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true'
-        url = dj_database_url.parse(url, conn_max_age=conn_max_age)
-
-        assert url['CONN_MAX_AGE'] == conn_max_age
-
-    def test_config_conn_max_age_setting(self):
-        conn_max_age = 600
-        os.environ['DATABASE_URL'] = 'mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true'
-        url = dj_database_url.config(conn_max_age=conn_max_age)
-
-        assert url['CONN_MAX_AGE'] == conn_max_age
-
-    def test_database_url_with_options(self):
-        # Test full options
-        os.environ['DATABASE_URL'] = 'postgres://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn?sslrootcert=rds-combined-ca-bundle.pem&sslmode=verify-full'
-        url = dj_database_url.config()
-
-        assert url['ENGINE'] == 'django.db.backends.postgresql_psycopg2'
-        assert url['NAME'] == 'd8r82722r2kuvn'
-        assert url['HOST'] == 'ec2-107-21-253-135.compute-1.amazonaws.com'
-        assert url['USER'] == 'uf07k1i6d8ia0v'
-        assert url['PASSWORD'] == 'wegauwhgeuioweg'
-        assert url['PORT'] == 5431
-        assert url['OPTIONS'] == {
-            'sslrootcert': 'rds-combined-ca-bundle.pem',
-            'sslmode': 'verify-full'
-        }
-
-        # Test empty options
-        os.environ['DATABASE_URL'] = 'postgres://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn?'
-        url = dj_database_url.config()
-        assert 'OPTIONS' not in url
-
-
-    def test_oracle_parsing(self):
+    def test_oracle(self):
         url = 'oracle://scott:tiger@oraclehost:1521/hr'
-        url = dj_database_url.parse(url)
+        url = connection_url.config(url)
 
         assert url['ENGINE'] == 'django.db.backends.oracle'
         assert url['NAME'] == 'hr'
@@ -192,9 +115,9 @@ class DatabaseTestSuite(unittest.TestCase):
         assert url['PASSWORD'] == 'tiger'
         assert url['PORT'] == 1521
 
-    def test_oracle_gis_parsing(self):
+    def test_oracle_gis(self):
         url = 'oraclegis://scott:tiger@oraclehost:1521/hr'
-        url = dj_database_url.parse(url)
+        url = connection_url.config(url)
 
         assert url['ENGINE'] == 'django.contrib.gis.db.backends.oracle'
         assert url['NAME'] == 'hr'
@@ -203,14 +126,14 @@ class DatabaseTestSuite(unittest.TestCase):
         assert url['PASSWORD'] == 'tiger'
         assert url['PORT'] == 1521
 
-    def test_oracle_dsn_parsing(self):
+    def test_oracle_dsn(self):
         url = (
             'oracle://scott:tiger@/'
             '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)'
             '(HOST=oraclehost)(PORT=1521)))'
             '(CONNECT_DATA=(SID=hr)))'
         )
-        url = dj_database_url.parse(url)
+        url = connection_url.config(url)
 
         assert url['ENGINE'] == 'django.db.backends.oracle'
         assert url['USER'] == 'scott'
@@ -226,15 +149,116 @@ class DatabaseTestSuite(unittest.TestCase):
 
         assert url['NAME'] == dsn
 
-    def test_oracle_tns_parsing(self):
+    def test_oracle_tns(self):
         url = 'oracle://scott:tiger@/tnsname'
-        url = dj_database_url.parse(url)
+        url = connection_url.config(url)
 
         assert url['ENGINE'] == 'django.db.backends.oracle'
         assert url['USER'] == 'scott'
         assert url['PASSWORD'] == 'tiger'
         assert url['NAME'] == 'tnsname'
         assert url['HOST'] == ''
+        assert url['PORT'] == ''
+
+    def test_engine_setting(self):
+        engine = 'django_mysqlpool.backends.mysqlpool'
+        url = 'mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true'
+        url = connection_url.config(url, ENGINE=engine)
+
+        assert url['ENGINE'] == engine
+
+    def test_conn_max_age_setting(self):
+        conn_max_age = 600
+        url = 'mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true'
+        url = connection_url.config(url, CONN_MAX_AGE=conn_max_age)
+
+        assert url['CONN_MAX_AGE'] == conn_max_age
+
+    def test_default_settings(self):
+        engine = 'django_mysqlpool.backends.mysqlpool'
+        conn_max_age = 600
+        url = 'mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true'
+        url = connection_url.config(url, {'ENGINE':engine, 'CONN_MAX_AGE': conn_max_age})
+
+        assert url['ENGINE'] == 'django.db.backends.mysql'
+        assert url['CONN_MAX_AGE'] == conn_max_age
+
+    def test_override_settings(self):
+        engine = 'mysql.connector.django'
+        conn_max_age = 600
+        url = 'mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true'
+        url = connection_url.config(url,
+                {'ENGINE':'django_mysqlpool.backends.mysqlpool', 'CONN_MAX_AGE': conn_max_age},
+                ENGINE=engine)
+
+        assert url['ENGINE'] == engine
+        assert url['CONN_MAX_AGE'] == conn_max_age
+
+    def test_default_options(self):
+        conn_max_age = 600
+        url = 'mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true'
+        url = connection_url.config(url, {'CONN_MAX_AGE': conn_max_age, 'OPTIONS': {'MAX_CONNECTIONS': 120, 'TIMEOUT': 100}})
+
+        assert url['CONN_MAX_AGE'] == conn_max_age
+        assert url['OPTIONS'] == {
+            'reconnect': 'true',
+            'MAX_CONNECTIONS': 120,
+            'TIMEOUT': 100,
+        }
+
+    def test_override_options(self):
+        url = 'mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true'
+        url = connection_url.config(url, {'OPTIONS': {'MAX_CONNECTIONS': 120}}, OPTIONS={'TIMEOUT': 100})
+
+        assert url['OPTIONS'] == {
+            'TIMEOUT': 100,
+        }
+
+    def test_database_url(self):
+        os.environ.pop('DATABASE_URL', None)
+        self.assertRaises(connection_url.ImproperlyConfigured, connection_url.config, 'DATABASE_URL')
+
+        os.environ['DATABASE_URL'] = 'postgres://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn'
+
+        url = connection_url.config('DATABASE_URL')
+
+        assert url['ENGINE'] == 'django.db.backends.postgresql_psycopg2'
+        assert url['NAME'] == 'd8r82722r2kuvn'
+        assert url['HOST'] == 'ec2-107-21-253-135.compute-1.amazonaws.com'
+        assert url['USER'] == 'uf07k1i6d8ia0v'
+        assert url['PASSWORD'] == 'wegauwhgeuioweg'
+        assert url['PORT'] == 5431
+
+    def test_database_url_with_options(self):
+        # Test full options
+        os.environ['DATABASE_URL'] = 'postgres://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn?sslrootcert=rds-combined-ca-bundle.pem&sslmode=verify-full'
+        url = connection_url.config('DATABASE_URL')
+
+        assert url['ENGINE'] == 'django.db.backends.postgresql_psycopg2'
+        assert url['NAME'] == 'd8r82722r2kuvn'
+        assert url['HOST'] == 'ec2-107-21-253-135.compute-1.amazonaws.com'
+        assert url['USER'] == 'uf07k1i6d8ia0v'
+        assert url['PASSWORD'] == 'wegauwhgeuioweg'
+        assert url['PORT'] == 5431
+        assert url['OPTIONS'] == {
+            'sslrootcert': 'rds-combined-ca-bundle.pem',
+            'sslmode': 'verify-full'
+        }
+
+        # Test empty options
+        os.environ['DATABASE_URL'] = 'postgres://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn?'
+        url = connection_url.config('DATABASE_URL')
+        assert 'OPTIONS' not in url
+
+    def test_cleardb(self):
+        url = 'mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true'
+        url = connection_url.config(url)
+
+        assert url['ENGINE'] == 'django.db.backends.mysql'
+        assert url['NAME'] == 'heroku_97681db3eff7580'
+        assert url['HOST'] == 'us-cdbr-east.cleardb.com'
+        assert url['USER'] == 'bea6eb025ca0d8'
+        assert url['PASSWORD'] == '69772142'
         assert url['PORT'] == ''
 
 
